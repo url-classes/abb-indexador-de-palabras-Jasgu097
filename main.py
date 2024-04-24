2fa4d30b5a9fb3302cb5049b47862d16ccda1979
Jasgu097 <gutierrezdeleonj@gmail.com>
2024-04-24 17:40:43 -0600

    implemetacio de los 10 textos

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from Binarytree import BinaryTree
+from listasdobles import TextEditor
+texto_1=TextEditor()
+texto_2=TextEditor()
+texto_3=TextEditor()
+texto_4=TextEditor()
+texto_5=TextEditor()
+texto_6=TextEditor()
+texto_7=TextEditor()
+texto_8=TextEditor()
+texto_9=TextEditor()
+texto_10=TextEditor()
 
+texto_1.load_file("texto 1.txt")
+texto_2.load_file("texto 2.txt")
+texto_3.load_file("texto 3.txt")
+texto_4.load_file("texto 4.txt")
+texto_5.load_file("texto 5.txt")
+texto_6.load_file("texto 6.txt")
+texto_7.load_file("texto 7.txt")
+texto_8.load_file("texto 8.txt")
+texto_9.load_file("texto 9.txt")
+texto_10.load_file("texto 10.txt")
+
+
+
+
+
+"""
 a=BinaryTree()
 a.insert("Hola")
 a.insert("a")
@@ -10,3 +37,4 @@ a.insert("estan")
 print("preorde", a.preorder())
 print("inorden", a.inorder())
 print("post", a.postorder())
+"""
--- node.py
+++ node.py
@@ -1,5 +1,11 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value, data):
+        #Arbol
         self.value = value
         self.left = None
         self.right = None
+class Node_lista:
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
